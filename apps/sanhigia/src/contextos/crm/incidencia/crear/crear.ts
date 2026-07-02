6cd4a5ef0c769cd49ddba017f68c4c9dcadb9ba6
Javier-Valero-yeboyebo <jvaola@gmail.com>
2026-07-02 10:57:11 +0200

    refact(incidencia): Actualizado seteo inicial para tipoIncidencia.

--- apps/sanhigia/src/contextos/crm/incidencia/crear/crear.ts
+++ apps/sanhigia/src/contextos/crm/incidencia/crear/crear.ts
@@ -12,7 +12,7 @@ export const nuevaIncidenciaVacia: NuevaIncidencia = {
     facturaId: "",
     codigoFactura: "",
     articuloId: "",
-    tipoIncidencia: "Proveedor",
+    tipoIncidencia: "Transportista",
     categoriaIncidencia: "INCIDT",
     subCategoriaIncidencia: ""
 };
